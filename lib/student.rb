2eb8839fbb7643cb04217a82eb578b6a734914fa
Ronda D Cowen <cowenhome5@gmail.com>
2020-02-26 00:11:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,12 +53,12 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10(number)
+  def self.first_X_students_in_grade_10(size)
     sql = <<-SQL
       SELECT COUNT(grade)
       FROM students
       WHERE grade = 10
-
+      LIMIT size
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
