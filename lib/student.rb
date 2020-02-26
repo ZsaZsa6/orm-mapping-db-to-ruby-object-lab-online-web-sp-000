de801bfa444932acff26467d52d52d129d875c02
Ronda D Cowen <cowenhome5@gmail.com>
2020-02-26 00:35:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,9 +62,8 @@ class Student
       LIMIT ?
 
     SQL
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
+    DB[:conn].execute(sql)
+    # end
   end
 
   def self.first_student_in_grade_10
