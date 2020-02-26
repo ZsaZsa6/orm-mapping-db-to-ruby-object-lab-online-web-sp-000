b448ceceb07d06f76806661b7b35c3f0024bcbb2
Ronda D Cowen <cowenhome5@gmail.com>
2020-02-26 00:39:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,10 +59,12 @@ class Student
       FROM students
       WHERE grade = 10
       ORDER BY id DESC
-      LIMIT (size)
+      LIMIT 
 
     SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.first_student_in_grade_10
