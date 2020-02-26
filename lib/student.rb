e41eb34decf5f6a29d14be46e149c34f705a8043
Ronda D Cowen <cowenhome5@gmail.com>
2020-02-26 00:10:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,7 +60,7 @@ class Student
       WHERE grade = 10
 
     SQL
-    DB[:conn].execute(sql, grade).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end.first
   end
